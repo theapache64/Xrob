73161d81bda13c61eeb1feef72fee01c9582e710
theapache64-bravo <theapache64@gmail.com>
2016-09-11 07:17:31 +0530

    Added method to return un-synchronized contacts

--- client/app/src/main/java/com/theah64/xrob/database/Contacts.java
+++ client/app/src/main/java/com/theah64/xrob/database/Contacts.java
@@ -2,9 +2,12 @@ package com.theah64.xrob.database;
 
 import android.content.Context;
 import android.database.ContentObserver;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.theah64.xrob.models.Contact;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,6 +15,7 @@ import java.util.List;
  */
 public class Contacts extends BaseTable<Contact> {
 
+    private static final String COLUMN_PHONE = "phone";
     private static Contacts instance;
 
     public static Contacts getInstance(final Context context) {
@@ -27,11 +31,37 @@ public class Contacts extends BaseTable<Contact> {
         super(context);
     }
 
+    /**
+     * Used to retrieve all un-synchronized contacts from local database.
+     */
+    public List<Contact> getAllUnSynced() {
 
-    @Override
-    public List<Contact> getAll() {
         List<Contact> contacts = null;
+        //Preparing query
+        final String query = "SELECT id,name,phone FROM contacts WHERE is_synced = 0;";
+
+        final SQLiteDatabase db = this.getReadableDatabase();
+        final Cursor cursor = db.rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+
+            contacts = new ArrayList<>(cursor.getCount());
+
+            do {
+                final String id = cursor.getString(cursor.getColumnIndex(COLUMN_ID));
+                final String name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME));
+                final String phone = cursor.getString(cursor.getColumnIndex(COLUMN_PHONE));
+
+                contacts.add(new Contact(id, name, phone, false));
+
+            } while (cursor.moveToNext());
+
+        }
+
+        cursor.close();
+        db.close();
 
         return contacts;
+
     }
 }
