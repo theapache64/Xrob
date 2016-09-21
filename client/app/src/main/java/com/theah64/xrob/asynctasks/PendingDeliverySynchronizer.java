e2cef31e70a43b3736d498c8daa69d1173f1c595
theapache64-bravo <theapache64@gmail.com>
2016-09-22 01:14:36 +0530

    Got to handle the pending delivery

--- client/app/src/main/java/com/theah64/xrob/asynctasks/PendingDeliverySynchronizer.java
+++ client/app/src/main/java/com/theah64/xrob/asynctasks/PendingDeliverySynchronizer.java
@@ -2,6 +2,15 @@ package com.theah64.xrob.asynctasks;
 
 import android.content.Context;
 
+import com.theah64.xrob.database.PendingDeliveries;
+import com.theah64.xrob.models.PendingDelivery;
+import com.theah64.xrob.utils.OkHttpUtils;
+
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 /**
  * Created by theapache64 on 21/9/16.
  */
@@ -14,6 +23,16 @@ public class PendingDeliverySynchronizer extends BaseJSONPostNetworkAsyncTask<Vo
     @Override
     protected Void doInBackground(String... strings) {
         //TODO : SQL to OK HTTP
+
+        final List<PendingDelivery> pendingDeliveryList = PendingDeliveries.getInstance(getContext()).getAll();
+        if (pendingDeliveryList != null) {
+            //Looping through each delivery.
+            for (final PendingDelivery pd : pendingDeliveryList) {
+
+                
+
+            }
+        }
         return null;
     }
 }
