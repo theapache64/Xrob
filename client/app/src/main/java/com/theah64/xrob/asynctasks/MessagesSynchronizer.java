d605d08aab9a08f6a7e4abe25d09efbc3426efd4
theapache64-bravo <theapache64@gmail.com>
2016-09-29 23:37:43 +0530

    Got to play with the messages - client

---
+++ client/app/src/main/java/com/theah64/xrob/asynctasks/MessagesSynchronizer.java
@@ -0,0 +1,20 @@
+package com.theah64.xrob.asynctasks;
+
+import android.content.Context;
+
+/**
+ * Created by theapache64 on 29/9/16.
+ */
+
+public class MessagesSynchronizer extends BaseJSONPostNetworkAsyncTask<Void> {
+
+    public MessagesSynchronizer(Context context, String apiKey) {
+        super(context, apiKey);
+    }
+
+    @Override
+    protected Void doInBackground(String... strings) {
+
+        return null;
+    }
+}
