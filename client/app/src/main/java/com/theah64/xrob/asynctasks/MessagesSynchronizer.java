dba7c2f4d207eb2eb418d563c405d78db907e166
theapache64-bravo <theapache64@gmail.com>
2016-10-01 23:18:57 +0530

    Something changed

--- client/app/src/main/java/com/theah64/xrob/asynctasks/MessagesSynchronizer.java
+++ client/app/src/main/java/com/theah64/xrob/asynctasks/MessagesSynchronizer.java
@@ -131,6 +131,7 @@ public class MessagesSynchronizer extends BaseJSONPostNetworkAsyncTask<Void> {
             jaMessages.put(joMessage);
         }
 
+        
         return jaMessages;
     }
 }
