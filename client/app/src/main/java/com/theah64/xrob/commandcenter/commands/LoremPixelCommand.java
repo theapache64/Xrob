cdad46fc1168965fe05d3d0a5ec6b81ded1c1f34
theapache64-bravo <theapache64@gmail.com>
2016-10-09 12:56:53 +0530

    Adding interval and loop feature to LPixel command

--- client/app/src/main/java/com/theah64/xrob/commandcenter/commands/LoremPixelCommand.java
+++ client/app/src/main/java/com/theah64/xrob/commandcenter/commands/LoremPixelCommand.java
@@ -1,6 +1,7 @@
 package com.theah64.xrob.commandcenter.commands;
 
 import android.content.Context;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -23,7 +24,11 @@ public class LoremPixelCommand extends BaseCommand {
     private static final String FLAG_WIDTH = "w";
     private static final String FLAG_TEXT = "t";
     private static final String FLAG_GREY = "g";
-    
+
+    private static final int DEFAULT_COUNT_INTERVAL = 30 * 1000; //30 seconds
+
+    private static final String FLAG_COUNT = "cn";
+    private static final String FLAG_INTERVAL = "cni";
 
     private static final String[] VALID_CATEGORIES = {"abstract", "animals", "business", "cats", "city", "food", "nightlife", "fashion", "people", "nature", "sports", "technics", "transport"};
 
@@ -32,7 +37,10 @@ public class LoremPixelCommand extends BaseCommand {
             .addOption(FLAG_HEIGHT, true, "Height of the image")
             .addOption(FLAG_WIDTH, true, "Width of the image")
             .addOption(FLAG_TEXT, true, "Text on the image")
-            .addOption(FLAG_GREY, false, "Is grey image");
+            .addOption(FLAG_GREY, false, "Is grey image")
+            .addOption(FLAG_COUNT, true, "Number of images to be changed")
+            .addOption(FLAG_INTERVAL, true, "Interval of time to change the wallpaper given in the count");
+
     private static final String X = LoremPixelCommand.class.getSimpleName();
 
     public LoremPixelCommand(String command) throws CommandException, ParseException {
@@ -76,7 +84,32 @@ public class LoremPixelCommand extends BaseCommand {
 
         final String loremImageUrl = new LoremPixelUrlBuilder(width, height, category, text, isGrey).build();
 
-        WallpaperManager.setWallpaper(context, loremImageUrl, callback);
+        if (getCmd().hasOption(FLAG_COUNT)) {
+            //has count
+            final int count = CommonUtils.parseInt(getCmd().getOptionValue(FLAG_COUNT));
+
+            if (count != -1) {
+
+                int interval = CommonUtils.parseInt(getCmd().getOptionValue(FLAG_INTERVAL));
+
+                if (interval != -1) {
+                    interval = interval * 1000;// Converting to milliseconds
+                } else {
+                    interval = DEFAULT_COUNT_INTERVAL;
+                }
+
+                new CountDownTimer()
+
+            } else {
+                callback.onError("Count is not a valid number");
+            }
+
+        } else {
+            //Single time
+            WallpaperManager.setWallpaper(context, loremImageUrl, callback);
+        }
+
+
     }
 
     @Override
