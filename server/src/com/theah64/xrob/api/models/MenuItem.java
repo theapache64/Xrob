8a04cb2ee1f7778a5c15f6e3bf36326cf038a3a9
theapache64@charlie <theapache64@gmail.com>
2016-10-11 23:58:32 +0530

    SERVER NEW command - hotdog

--- server/src/com/theah64/xrob/api/models/MenuItem.java
+++ server/src/com/theah64/xrob/api/models/MenuItem.java
@@ -6,13 +6,18 @@ package com.theah64.xrob.api.models;
 public class MenuItem {
 
     private final String title;
-    private final int count;
+    private final int menuId,count;
     private final String link;
 
-    public MenuItem(String title, int count, String link) {
+    public MenuItem(int menuId, String title, int count, String link) {
+        this.menuId = menuId;
         this.title = title;
         this.count = count;
         this.link = link;
+    }
+
+    public int getMenuId() {
+        return menuId;
     }
 
     public String getTitle() {
