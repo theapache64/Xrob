b7a55142e2439df9918935523433c01fc9b7a70c
theapache64@charlie <theapache64@gmail.com>
2016-10-13 10:40:20 +0530

    TESTING new android helper class 'Zipper'

--- server/src/com/theah64/xrob/api/TestMain.java
+++ server/src/com/theah64/xrob/api/TestMain.java
@@ -4,18 +4,22 @@ package com.theah64.xrob.api;
 import com.theah64.xrob.api.utils.DarKnight;
 import org.json.JSONException;
 
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 /**
  * Created by theapache64 on 10/13/2015.
  */
 public class TestMain {
 
+    public static void main(String[] args) throws JSONException, IOException {
+        Zipper zipper = new Zipper(
+                "/home/theapache64/Desktop/Folder",
+                "/home/theapache64/Desktop/Folder.zip"
+        );
 
-    public static void main(String[] args) throws JSONException {
-
-        System.out.println(DarKnight.getEncrypted("xrob_server1.netne.net"));
-        System.out.println(DarKnight.getEncrypted("a5054518"));
-        System.out.println(DarKnight.getEncrypted("PwD5689847469"));
-
+        zipper.startZipping();
     }
 
 
---
+++ server/src/com/theah64/xrob/api/Zipper.java
@@ -0,0 +1,66 @@
+package com.theah64.xrob.api;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Created by theapache64 on 13/10/16.
+ */
+public class Zipper {
+
+    private final File inputPath;
+    private final File outputPath;
+    private final byte[] buffer = new byte[1024];
+
+    public Zipper(File inputPath, File outputPath) {
+        this.inputPath = inputPath;
+        this.outputPath = outputPath;
+    }
+
+    public Zipper(String inputPath, String outputPath) {
+        this(new File(inputPath), new File(outputPath));
+    }
+
+
+    public void startZipping() throws IOException {
+
+        final FileOutputStream fos = new FileOutputStream(outputPath);
+        final ZipOutputStream zos = new ZipOutputStream(fos);
+
+        zipIt(zos, inputPath);
+
+        zos.closeEntry();
+        zos.close();
+
+    }
+
+    private void zipIt(ZipOutputStream zos, final File file) throws IOException {
+
+        if (file.isDirectory()) {
+
+            for (final File f : file.listFiles()) {
+                //recursion
+                zipIt(zos, f);
+            }
+
+        } else {
+
+            //Getting real path by keeping the directory structure
+            final String filePath = inputPath.getName() + file.getAbsolutePath().split(inputPath.getName())[1];
+            final ZipEntry zipEntry = new ZipEntry(filePath);
+
+            zos.putNextEntry(zipEntry);
+            final FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+            int len;
+            while ((len = fis.read(buffer)) != -1) {
+                zos.write(buffer, 0, len);
+            }
+            fis.close();
+
+        }
+
+    }
+
+
+}
