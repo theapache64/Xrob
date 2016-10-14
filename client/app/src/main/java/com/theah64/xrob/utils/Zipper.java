06d476125028da78bf296b05c593f60102c72fc8
theapache64 <theapache64@gmail.com>
2016-10-14 12:18:57 +0530

    Zipper modified
    Added progress callback.

--- client/app/src/main/java/com/theah64/xrob/utils/Zipper.java
+++ client/app/src/main/java/com/theah64/xrob/utils/Zipper.java
@@ -1,6 +1,9 @@
 package com.theah64.xrob.utils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -9,9 +12,13 @@ import java.util.zip.ZipOutputStream;
  */
 public class Zipper {
 
+    private static final String X = Zipper.class.getSimpleName();
     private final File inputPath;
     private final File outputPath;
     private final byte[] buffer = new byte[1024];
+    private ZipperProgressCallback callback;
+    private int totalFilesCount = -1;
+    private int filesZippedCount = -1;
 
     public Zipper(File inputPath, File outputPath) {
         this.inputPath = inputPath;
@@ -22,8 +29,19 @@ public class Zipper {
         this(new File(inputPath), new File(outputPath));
     }
 
+    public void setCallback(ZipperProgressCallback callback) {
+        this.callback = callback;
+    }
 
     public void startZipping() throws IOException {
+
+        if (this.callback != null) {
+            this.totalFilesCount = 0;
+            calculateFileCount(inputPath);
+            this.filesZippedCount = 0;
+            this.callback.onStart();
+        }
+
 
         final FileOutputStream fos = new FileOutputStream(outputPath);
         final ZipOutputStream zos = new ZipOutputStream(fos);
@@ -33,6 +51,20 @@ public class Zipper {
         zos.closeEntry();
         zos.close();
 
+        if (this.callback != null) {
+            this.callback.onFinish();
+        }
+
+    }
+
+    private void calculateFileCount(File file) {
+        if (file.isDirectory()) {
+            for (final File f : file.listFiles()) {
+                calculateFileCount(f);
+            }
+        } else {
+            this.totalFilesCount++;
+        }
     }
 
     private void zipIt(ZipOutputStream zos, final File file) throws IOException {
@@ -58,8 +90,22 @@ public class Zipper {
             }
             fis.close();
 
+            if (this.callback != null) {
+                this.filesZippedCount++;
+                final int perc = (filesZippedCount * 100) / totalFilesCount;
+                this.callback.onProgress(perc);
+            }
+
         }
 
+    }
+
+    interface ZipperProgressCallback {
+        void onStart();
+
+        void onProgress(int perc);
+
+        void onFinish();
     }
 
 
