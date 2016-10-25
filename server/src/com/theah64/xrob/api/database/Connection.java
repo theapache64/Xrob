5fcd7caef304bc5124ba8dc852f0f6c47958c6e9
theapache64@charlie <theapache64@gmail.com>
2016-10-25 20:19:41 +0530

    Database connection modified

--- server/src/com/theah64/xrob/api/database/Connection.java
+++ server/src/com/theah64/xrob/api/database/Connection.java
@@ -22,7 +22,7 @@ public class Connection {
             if (ds == null) {
                 final Context initContext = new InitialContext();
                 Context envContext = (Context) initContext.lookup("java:/comp/env");
-                ds = (DataSource) envContext.lookup(debugMode ? "jdbc/xrob" : "jdbc/MySQLDS");
+                ds = (DataSource) envContext.lookup(debugMode ? "jdbc/xrobLocal" : "jdbc/xrobRemote");
             }
 
             return ds.getConnection();
