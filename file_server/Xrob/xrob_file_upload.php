f4b06ccfb984cb9fd57dbd5cf0d5fbda27f12a12
theapache64@charlie <theapache64@gmail.com>
2016-10-26 09:53:04 +0530

    File upload script added

--- file_server/Xrob/xrob_file_upload.php
+++ file_server/Xrob/xrob_file_upload.php
@@ -7,8 +7,6 @@ $headers = apache_request_headers();
 $authKey = isset($headers['Authorization']) ? $headers['Authorization']  : null;
 header('Content-Type: application/json');
 
-define ('SITE_ROOT', dirname(__DIR__));
-
 $arr = array();
 
 if($authKey===SECRET){
@@ -19,12 +17,12 @@ if($authKey===SECRET){
 		$fileName = $_FILES['file']['name'];
 		$tmpName = $_FILES['file']['tmp_name'];
 
-		$target_file = SITE_ROOT. basename($_FILES["file"]["name"]);
+		$target_file =  "uploads/".basename($_FILES["file"]["name"]);
 
 		if(move_uploaded_file($tmpName, $target_file)){
 			$arr['error'] = true;
-			$arr['message'] = "file missing";
-			$arr['download_link'] = "http://".$_SERVER['SERVER_NAME'].$target_file;
+			$arr['message'] = "file uploaded";
+			$arr['download_link'] = "http://".$_SERVER['SERVER_NAME']."/Xrob/".$target_file;
 		}else{
 			$arr['error'] = true;
 			$arr['message'] = "failed to store the file";
